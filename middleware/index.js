eff27eef4d19ac0be72ea4457beff978d6a6d87b
Aniket093003 <aniketkainth98105@gmail.com>
2024-09-17 23:22:11 +0530

    middleware add

--- middleware/index.js
+++ middleware/index.js
@@ -1,21 +1,32 @@
+import { log } from 'console';
 import express from 'express'
 
 const app = express();
 
-app.get("/sum", function(req, res) {
+let reqCount = 0;
+
+function check(req, res, next){
+    reqCount += 1;
+    console.log("total number of requests = " + reqCount);
+    req.reqCount = reqCount;
+    next();
+}
+
+app.get("/sum",check , function(req, res) {
     const a = req.query.a;
     const b = req.query.b;
+    console.log("total number of requests = " + reqCount);
 
     res.json({
-        ans: a + b
+        ans: a + b,
     })
 });
 
-app.get("/multiply", function(req, res) {
+app.get("/multiply",check , function(req, res) {
     const a = req.query.a;
     const b = req.query.b;
     res.json({
-        ans: a * b
+        ans: a * b,
     })
 });
 
@@ -23,7 +34,7 @@ app.get("/divide", function(req, res) {
     const a = req.query.a;
     const b = req.query.b;
     res.json({
-        ans: a / b
+        ans: a / b,
     })
 
 });
